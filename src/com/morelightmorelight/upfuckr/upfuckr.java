53edb644a321f91dd06ac385aa86ee93b26c9adf
Matt Katz <gitorious@morelightmorelight.com>
2010-06-08 22:07:52 -0400

    wahey - begun with some command line magic
    android create project --name upfuckr -t 1 -p . -k com.morelightmorelight.upfuckr -a upfuckr

---
+++ src/com/morelightmorelight/upfuckr/upfuckr.java
@@ -0,0 +1,15 @@
+package com.morelightmorelight.upfuckr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class upfuckr extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
