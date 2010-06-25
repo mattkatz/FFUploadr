972435e79e1ef142fcf46074eaf2f91b366c231f
Matt Katz <gitorious@morelightmorelight.com>
2010-06-24 21:52:38 -0400

    adding in the ability to enter credentials

--- src/com/morelightmorelight/upfuckr/upfuckr.java
+++ src/com/morelightmorelight/upfuckr/upfuckr.java
@@ -2,14 +2,45 @@ package com.morelightmorelight.upfuckr;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
 
 public class upfuckr extends Activity
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+  private static final int ADD_ID = Menu.FIRST;
+  private static final int ACTIVITY_CREATE = 0;
+
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState)
+  {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.main);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu)
+  {
+    super.onCreateOptionsMenu(menu);
+    menu.add(0,ADD_ID,0,R.string.add_site);
+    return true;
+  }
+
+  @Override
+  public boolean onMenuItemSelected(int featureId, MenuItem item)
+  {
+    switch(item.getItemId()){
+      case ADD_ID:
+        add_site();
+        return true;
     }
+    return super.onMenuItemSelected(featureId,item);
+  }
+
+  private void add_site(){
+    Intent i = new Intent(this, Credentials.class);
+    startActivityForResult(i,ACTIVITY_CREATE);
+  }
+  
 }
