74e49780014d46dd3206770c4a3386bd85afd952
Matt Katz <gitorious@morelightmorelight.com>
2010-06-25 15:57:02 -0400

    change some text, begin integrating ftp
    currently trying out using lysesoft's andftp intent

--- src/com/morelightmorelight/upfuckr/upfuckr.java
+++ src/com/morelightmorelight/upfuckr/upfuckr.java
@@ -4,7 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.content.Intent;
+import android.net.Uri;
+import android.widget.Button;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 public class upfuckr extends Activity
 {
@@ -17,6 +22,16 @@ public class upfuckr extends Activity
   {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.main);
+      Button uploadPic = (Button) findViewById(R.id.upload);
+      uploadPic.setOnClickListener(new View.OnClickListener(){
+        public void onClick(View view){
+          setResult(RESULT_OK);
+          //I dunno, upload?
+          upload();
+        }
+      });
+
+
   }
 
   @Override
@@ -43,6 +58,40 @@ public class upfuckr extends Activity
     //startActivityForResult(i,ACTIVITY_CREATE);
     startActivity(new Intent(this,Prefs.class));
     
+  }
+
+  private void upload()
+  {
+    //get our shared preferences
+    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+    String host = prefs.getString("host","");
+    String path = prefs.getString("path","");
+    String user = prefs.getString("user","");
+    String pass = prefs.getString("pass","");
+
+    Intent intent = new Intent();
+    intent.setAction(Intent.ACTION_PICK);
+    // FTP URL (Starts with ftp://, sftp:// or ftps:// followed by hostname and port).
+    Uri ftpUri = Uri.parse("ftp://"+host+":21");
+    intent.setDataAndType(ftpUri, "vnd.android.cursor.dir/lysesoft.andftp.uri");
+    // // FTP credentials (optional)
+    intent.putExtra("ftp_username", user);
+    intent.putExtra("ftp_password", pass);
+    //intent.putExtra("ftp_keyfile", "/sdcard/dsakey.txt");
+    //intent.putExtra("ftp_keypass", "optionalkeypassword");
+    // FTP settings (optional)
+    intent.putExtra("ftp_pasv", "true");
+    //intent.putExtra("ftp_resume", "true");
+    //intent.putExtra("ftp_encoding", "UTF8");
+    // Upload
+    intent.putExtra("command_type", "upload");
+    // Activity title
+    intent.putExtra("progress_title", "Uploading files ...");
+    intent.putExtra("local_file1", "/sdcard/subfolder1/file1.zip");
+    intent.putExtra("local_file2", "/sdcard/subfolder2/file2.zip");
+    // Optional initial remote folder (it must exist before upload)
+    intent.putExtra("remote_folder", path);
+    startActivityForResult(intent, 1);
   }
   
 }
