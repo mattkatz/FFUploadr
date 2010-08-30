e358e3ef5231e2c40650e21c820a52a8e7674ffd
Matt Katz <gitorious@morelightmorelight.com>
2010-08-29 20:22:31 -0400

    extracting upload to a new activity

---
+++ src/com/morelightmorelight/upfuckr/uploadr.java
@@ -0,0 +1,23 @@
+package com.morelightmorelight.upfuckr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import android.view.View;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.Button;
+import android.graphics.drawable.Drawable;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.database.Cursor;
+import android.provider.MediaStore;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.apache.commons.net.ftp.*;
+import java.io.*;
+
+import android.util.Log;
