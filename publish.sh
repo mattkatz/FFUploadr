6b1ada94f8bfacf5cd611408b3cdfb7b26a6a859
Matt Katz <gitorious@morelightmorelight.com>
2010-12-08 00:32:37 -0500

    fixed publish to be dependent on current branch

--- publish.sh
+++ publish.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+curbranch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 pushd bugs
 ditz html
 popd
+git add bugs
 git commit bugs -m "auto committing ditz html files"
 git checkout gh-pages
-git checkout master bugs/html
+git checkout $curbranch bugs/html
+git add bugs
 git commit bugs -m "auto committing ditz html files"
 git push
-git checkout master
+git checkout $curbranch
 
